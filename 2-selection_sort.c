cefa2e8bcf03bb41091119649bdec084a24f2c87
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-06-23 15:50:18 +0100

    selection sor implemenatation

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,30 +1,5 @@
 #include "sort.h"
 
-size_t smallest(int *array, size_t k, size_t size);
-
-/**
- * smallest - Get the smallest element in array
- * @array: pointer to the array
- * @k: index
- * @size: the size of the array
- * Return: The position of the smallest element
- */
-
-/*size_t smallest(int *array, size_t k, size_t size)
-{
-	size_t pos = k, i;
-	int small = array[k];
-
-	for (i = k + 1; i < size; i++)
-	{
-		if (array[i] < small)
-		{
-			small = array[i];
-			pos = i;
-		}
-	}
-	return (pos);
-}*/
 
 /**
  * selection_sort - implementation of selection sort
@@ -32,63 +7,31 @@ size_t smallest(int *array, size_t k, size_t size);
  * @size: size of array;
  * Return: void
  */
-/*
-void selection_sort(int *array, size_t size)
-{
-	size_t k, pos, temp;
-
-	for (k = 0; k < size; k++)
-	{
-		pos = smallest(array, k, size);
-		temp = array[k];
-		array[k] = array[pos];
-		array[pos] = temp;
-		print_array(array, size);
-	}
-}
-*/
-
-
-void swap(int *a, int *b);
-
-/**
- * swap - swap to element in array
- * @a: first element
- * @b: second element;
- * Return
- */
-
-void swap(int *a, int *b)
-{
-	int temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
 
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min;
+	int temp;
 
 	if (!array || size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[i])
+			if (array[j] < array[min])
 			{
 				min = j;
 			}
 		}
 		if (array[min] != array[i] || min != i)
 		{
-			swap(&array[min], &array[i]);
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
 			print_array(array, size);
 		}
-
 	}
 }
